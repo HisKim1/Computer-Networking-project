581884617d908fdd7a9544e4386261ad441829aa
HisKim1 <gmltn1323@gmail.com>
2022-11-10 15:24:48 +0900

    22.11.10

--- client.py
+++ client.py
@@ -5,19 +5,32 @@ import os
 BUFF_SIZE = 1024
 
 
-# parameter :
-# function : get python file from user's local computer / return file as struct
-def getSourceCode(client_socket):
-    file_name = input('.py file name : ')
+# parameter : client_socket = socket already created. use this socket to send .py
+# function : get python file from user's local computer
+def sendSourceCode(client_socket):
+    file_name = input('.py file path : ')
+
+    # if there does not exist such file
+    while not os.path.exists(os.path.abspath(file_name)):
+        file_name = input('there is no file. type again.')
+
+    # getsize() return certain file size of byte
     FILE_SIZE = os.path.getsize(file_name)
+
     FILE_SIZE = struct.pack('L', FILE_SIZE)
 
+    # file을 읽어서 BUFF_SIZE만큼씩만 보낸다... struct는 어디서 쓰인 것일까...
     f = open(file_name, 'rb')
     l = f.read(BUFF_SIZE)
 
+    # just for counting # of packets
+    i = 1
+
     while l:
+        print(f'{i}번째 packet')
         client_socket.send(l)
         l = f.read(BUFF_SIZE)
+        i += 1
     f.close()
 
 
@@ -26,46 +39,54 @@ def getSourceCode(client_socket):
 def setConnection():
     client_socket = socket.socket()
     print('socket created')
+
     client_address = getServerInfo()
     print(client_address)
 
-    # socket.connect parameter 'address' = (hostname, port) tuple로 제공해야 함
     try:
+        print('start to connect')
+        os.system("pause")
+
+        # error type of connect() : TimeoutError, InterruptedError
         client_socket.connect(client_address)
-    except ConnectionError :
-        print(ConnectionError)
-
-
-    # 일단 제외,,,
-    # getSourceCode(client_socket)
+    except Exception as ex:
+        print(f'exception occured : {ex}')
 
     while True:
+        sendSourceCode(client_socket)
 
-        #-------.py file로 바꿔야 할 부분--------
-        message = input("any message to sent?")
-
-        if not message:
-            break
-        client_socket.send(message.encode())
+        #-------sample : message 주고 받기--------
+        # message = input("any message to sent?")
+        #
+        # if not message:
+        #     break
+        # client_socket.send(message.encode())
         #--------------------------------------
 
         data = client_socket.recv(1024)
-        print("from server: ", data.decode())
+        print("from server : ", data.decode())
+
+        retry = input('Try again? (Y/N) : ')
+        if retry == 'Y' or retry == 'Y'.lower():
+            continue
+        else:
+            break
 
     client_socket.close()
 
 
 # parameter :
-# function : return server IP and address as tuple
+# function : return server IP in string and port in int as tuple
 def getServerInfo():
     dip = input('Server ip : ')
     dport = input('Server port to connect : ')
 
-    # Just for test -> 왜 IP로 넣으면 안되고 이름으로 넣어야 될까,,,
-    dip = socket.gethostname()
-    dport = 12121
+    # socket의 address에는 (hostname=str, port=int), (IPv4 address=str, port=int) 둘 다 가능함.
 
-    return dip, dport
+    # gethostbyaddr(ip) returns (hostname, alias name, IP address)
+    # dip = socket.gethostbyaddr(dip)
+
+    return dip, int(dport)
 
 
 # 스크립트를 실행하려면 여백의 녹색 버튼을 누릅니다.
--- server.py
+++ server.py
@@ -3,29 +3,47 @@ import socket
 
 
 def createSocket():
+
+    # set host IP and port
     HOST_NAME = socket.gethostname()
     HOST_IP = socket.gethostbyname(HOST_NAME)
     HOST_PORT = 12121
 
+    print("hostNAME :", HOST_NAME)
     print('hostIP :', HOST_IP)
     print('hostPORT : ', HOST_PORT)
 
+
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # to prevent the error caused by occupied port by other clients
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
     server_socket.bind((HOST_IP, HOST_PORT))
 
+    # 사람 늘릴려면 바꿔줘야 함.
     server_socket.listen(1)
 
     client_socket, client_addr = server_socket.accept()
     print('connection established')
 
     while True:
+        # .py 파일이면 여기도 바꿔야 됨. while로 struct 돌려가면서 받기
+        # 하지만 그냥 받아도 돌아가긴 함?!! -> .py 파일이 아직 작아서 그런 듯.
         data = client_socket.recv(1024)
 
+        # if empty file received, send error message
+        if not data:
+            client_socket.send('Empty file. Try again')
+            continue
+
+        # source processing part
+
+        #show received packet
         print('from client ', client_addr, data.decode())
 
-        result = input('Well done! any message?')
+        # after processing, result (success | fail) message send
+        result = input('student\'s result : ')
         client_socket.send(result.encode())
 
     client_socket.close()
